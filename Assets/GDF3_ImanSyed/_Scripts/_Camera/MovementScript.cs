eb3f55b1f5eda5fb0db96e42c727a8404f4baa6d
Iman <31649603+ImanSyed@users.noreply.github.com>
2017-09-05 21:34:56 +0530

    Platform Generation
    Currently bugged

---
+++ Assets/GDF3_ImanSyed/_Scripts/_Camera/MovementScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+	public int restrictionDuration = 2;
+	int restriction = 0;
+	public float speed = 5;
+	public float jumpSpeed = 5;
+	float gravity = 50;
+	private Vector3 moveDirection = Vector3.zero;
+	bool flag;
+	float t;
+	public GameObject platform;
+
+
+	void Update () {
+		
+		t += Time.deltaTime;
+		if (t >= restrictionDuration) {
+			flag = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Space");
+			if (flag) {
+				Debug.Log ("Free");
+				restriction++;
+				flag = false;
+				t = 0;
+				Vector3 v = transform.position;
+				if (restriction == 1) {
+					v.y = v.y - 75f;
+					platform.transform.position = v;
+					platform.SetActive (true);
+					gameObject.GetComponent<Rigidbody> ().useGravity = true;
+				}
+				if (restriction == 2) {
+					platform.transform.position = v;
+					v = Vector3.one;
+					v.x = 35;
+					v.z = 5;
+					platform.transform.localScale = v;
+				}
+			}
+		}
+
+		if (restriction == 0) {
+			CharacterController controller = GetComponent<CharacterController> ();
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+			controller.Move (moveDirection * Time.deltaTime);
+		} else {
+			CharacterController controller = GetComponent<CharacterController> ();
+			if (controller.isGrounded) {
+				moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+				moveDirection = transform.TransformDirection (moveDirection);
+				moveDirection *= speed;
+			}
+			moveDirection.y -= gravity * Time.deltaTime;
+			controller.Move (moveDirection * Time.deltaTime);
+		}
+	}
+		
+}
