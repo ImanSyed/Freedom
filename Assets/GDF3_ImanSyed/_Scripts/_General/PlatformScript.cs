b9a68045a309516056d9118cf4600d1f325ebc61
Iman <31649603+ImanSyed@users.noreply.github.com>
2017-09-06 19:13:16 +0530

    ok

--- Assets/GDF3_ImanSyed/_Scripts/_General/PlatformScript.cs
+++ Assets/GDF3_ImanSyed/_Scripts/_General/PlatformScript.cs
@@ -4,41 +4,13 @@ using UnityEngine;
 
 public class PlatformScript : MonoBehaviour {
 
-	public GameObject Plat;
-	public GameObject spawnLeft;
-	public GameObject spawnRight;
-	public GameObject spawnTop;
-	public GameObject spawnBottom;
-
-
-
-	void OnTriggerEnter(Collider col){
-		if (col.gameObject.tag == "Player") {;
-			if (transform.localPosition.x > 0) {
-				Debug.Log ("Right");
-				GameObject ob = (GameObject)Instantiate (Plat, spawnRight.transform.position, Plat.transform.rotation);
-				Destroy(ob.transform.Find("Trigger Left").gameObject);
-
-			} 
-			else if(transform.localPosition.x < 0) {
-				Debug.Log ("Left");
-				GameObject ob = (GameObject)Instantiate (Plat, spawnLeft.transform.position, Plat.transform.rotation);
-				Destroy(ob.transform.Find("Trigger Right").gameObject);
-
-			}
-			else if(transform.localPosition.z < 0) {
-				Debug.Log ("Bottom");
-				GameObject ob = (GameObject)Instantiate (Plat, spawnBottom.transform.position, Plat.transform.rotation);
-				Destroy(ob.transform.Find("Trigger Top").gameObject);
-
-			}
-			else if(transform.localPosition.z > 0) {
-				Debug.Log ("Top");
-				GameObject ob = (GameObject)Instantiate (Plat, spawnTop.transform.position, Plat.transform.rotation);
-				Destroy(ob.transform.Find("Trigger Bottom").gameObject);
-
-			}
-			Destroy (gameObject);
-		}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
